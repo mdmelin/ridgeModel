1d43d10552b66463314dc9e5830799fdc25f18a9
Max Melin <mmelin@g.ucla.edu>
2023-02-28 13:20:59 -0800

    cleanup

--- ridgeModel_refactor/sandbox.m
+++ ridgeModel_refactor/sandbox.m
@@ -3,17 +3,17 @@ clc;clear all;close all;
 addpath('C:\Data\churchland\ridgeModel\Max_Analysis')
 addpath('C:\Data\churchland\ridgeModel\widefield')
 
-%cPath = 'X:\Widefield'; animals = {'mSM63','mSM64','mSM65','mSM66'};
-cPath = 'Y:\Widefield'; animals = {'CSP22','CSP23','CSP32','CSP38'};
+cPath = 'X:\Widefield'; animals = {'mSM63','mSM64','mSM65','mSM66'};
+%cPath = 'Y:\Widefield'; animals = {'CSP22','CSP23','CSP32','CSP38'};
 
 %glmFile = 'allaudio_detection.mat';
-%glmPath = 'X:\Widefield\glm_hmm_models\global_model_map.mat';
-glmPath = 'X:\Widefield\glm_hmm_models\global_model_map_csp.mat';
+glmPath = 'X:\Widefield\glm_hmm_models\global_model_map.mat';
+%glmPath = 'X:\Widefield\glm_hmm_models\global_model_map_csp.mat';
 %sessiondates = getGLMHMMSessions(cPath,animals,glmFile); %get sessions with GLM-HMM data
 sessiondates = getGlobalGLMHMMSessions(glmPath); %get sessions with GLM-HMM data
 
 %% Retrain models over different states
-diary on
+
 %runRidge_overStates(cPath,'CSP22','23-Jun-2020',glmPath);
 
 for i = 1:length(animals)
@@ -22,7 +22,7 @@ for i = 1:length(animals)
         runRidge_overStates(cPath,animals{i},sessiondates{i}{j},glmPath);
     end
 end
-diary off
+
 
 %% Now plot those results
 fileprefix = '';
@@ -121,110 +121,4 @@ ylabel('deltaR^2');
 xlabel('Time from handle grab (s)')
 legend({'','','Engaged','','','Disengaged','','','',''})
 
-
 %exportgraphics(gcf,'C:\Data\churchland\PowerpointsPostersPresentations\SFN2022/FridayUpdate\encodingmodel\fullcvr.pdf');
-
-%% now shuffle the state labels
-for i = 1:length(animals)
-    for j = 1:length(sessiondates{i})
-        fprintf('\nRunning for %s, %s.\n\n',animals{i},sessiondates{i}{j});
-        runRidge_overStates_shuffletrials(cPath,animals{i},sessiondates{i}{j},glmFile); %shuffle state labels randomly
-    end
-end
-
-%%
-runRidge_overStates_shuffletrials(cPath,animals{4},sessiondates{4}{5},glmFile); %shuffle state labels randomly
-
-%% plot that
-counter = 1;
-for i = 1:length(animals)
-    for j = 1:length(sessiondates{i})
-        fulla(counter,:) = returnVarianceMovie(animals{i},sessiondates{i}{j},'SfullA.mat');
-        sponta(counter,:) = returnVarianceMovie(animals{i},sessiondates{i}{j},'SspontA.mat');
-        opa(counter,:) = returnVarianceMovie(animals{i},sessiondates{i}{j},'SoperantA.mat');
-        taskvara(counter,:) = returnVarianceMovie(animals{i},sessiondates{i}{j},'StaskA.mat');
-        nosponta(counter,:) = returnVarianceMovie(animals{i},sessiondates{i}{j},'SnospontA.mat');
-        noopa(counter,:) = returnVarianceMovie(animals{i},sessiondates{i}{j},'SnooperantA.mat');
-        notaskvara(counter,:) = returnVarianceMovie(animals{i},sessiondates{i}{j},'SnotaskA.mat');
-
-        fullb(counter,:) = returnVarianceMovie(animals{i},sessiondates{i}{j},'SfullB.mat');
-        spontb(counter,:) = returnVarianceMovie(animals{i},sessiondates{i}{j},'SspontB.mat');
-        opb(counter,:) = returnVarianceMovie(animals{i},sessiondates{i}{j},'SoperantB.mat');
-        taskvarb(counter,:) = returnVarianceMovie(animals{i},sessiondates{i}{j},'StaskB.mat');
-        nospontb(counter,:) = returnVarianceMovie(animals{i},sessiondates{i}{j},'SnospontB.mat');
-        noopb(counter,:) = returnVarianceMovie(animals{i},sessiondates{i}{j},'SnooperantB.mat');
-        notaskvarb(counter,:) = returnVarianceMovie(animals{i},sessiondates{i}{j},'SnotaskB.mat');
-
-        counter = counter + 1;
-        fprintf('\ncounter is %i\n',counter);
-    end
-end
-
-dsponta = fulla-nosponta;
-dspontb = fullb-nospontb;
-dtaska = fulla-notaskvara;
-dtaskb = fullb-notaskvarb;
-dopa = fulla-noopa;
-dopb = fullb-noopb;
-
-
-time = linspace(0,5,size(fulla,2));
-time = time-time(30);
-%%
-figure; hold on;
-title('Full Model')
-stdshade(fulla,.2,'red',time,6,[30],[]);
-stdshade(fullb,.2,'blue',time,6,[30],[]);
-ylabel('cvR^2');
-xlabel('Time from handle grab (s)')
-legend({'','','Engaged','','','Disengaged','','','',''})
-
-figure; hold on;
-title('Task Variables')
-stdshade(taskvara,.2,'red',time,6,[30],[]);
-stdshade(taskvarb,.2,'blue',time,6,[30],[]);
-ylabel('cvR^2');
-xlabel('Time from handle grab (s)')
-legend({'','','Engaged','','','Disengaged','','','',''})
-
-figure; hold on;
-title('Operant Motor Variables')
-stdshade(opa,.2,'red',time,6,[30],[]);
-stdshade(opb,.2,'blue',time,6,[30],[]);
-ylabel('cvR^2');
-xlabel('Time from handle grab (s)')
-legend({'','','Engaged','','','Disengaged','','','',''})
-
-figure; hold on;
-title('Spontaneous Motor Variables')
-stdshade(sponta,.2,'red',time,6,[30],[]);
-stdshade(spontb,.2,'blue',time,6,[30],[]);
-ylabel('cvR^2');
-xlabel('Time from handle grab (s)')
-legend({'','','Engaged','','','Disengaged','','','',''})
-
-
-
-figure; hold on;
-title('Task Variables')
-stdshade(dtaska,.2,'red',time,6,[30],[]);
-stdshade(dtaskb,.2,'blue',time,6,[30],[]);
-ylabel('deltaR^2');
-xlabel('Time from handle grab (s)')
-legend({'','','Engaged','','','Disengaged','','','',''})
-
-figure; hold on;
-title('Operant Motor Variables')
-stdshade(dopa,.2,'red',time,6,[30],[]);
-stdshade(dopb,.2,'blue',time,6,[30],[]);
-ylabel('deltaR^2');
-xlabel('Time from handle grab (s)')
-legend({'','','Engaged','','','Disengaged','','','',''})
-
-figure; hold on;
-title('Spontaneous Motor Variables')
-stdshade(dsponta,.2,'red',time,6,[30],[]);
-stdshade(dspontb,.2,'blue',time,6,[30],[]);
-ylabel('deltaR^2');
-xlabel('Time from handle grab (s)')
-legend({'','','Engaged','','','Disengaged','','','',''})
