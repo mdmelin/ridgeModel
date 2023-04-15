3e7c067d7a61ab304604aed8453249492f2ba514
Max Melin <mmelin@g.ucla.edu>
2023-04-14 18:54:30 -0700

    Faster loading

--- ridgeModel_refactor/returnVariance.m
+++ ridgeModel_refactor/returnVariance.m
@@ -1,15 +1,15 @@
-function out = returnVarianceMovie(cpath,mouse,rec,modelfile)
+function out = returnVariance(cpath,mouse,rec,modelfile)
 datapath = [cpath filesep mouse filesep 'SpatialDisc' filesep rec filesep];
 load('C:\Data\churchland\ridgeModel\widefield\allenDorsalMapSM.mat','dorsalMaps'); %Get allen atlas
 try
-    load([datapath modelfile],'cMovie');
+    load([datapath modelfile],'cMap');
 catch
     fprintf('\nThe encoding model file does not exist!');
     out = NaN;
 return
 end
 
-out = double(cMovie);
-out = mean(out,1);
+out = double(cMap);
+out = mean(out);
 
 end
